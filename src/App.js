72007f1c40c7001612ed7db89ff6094f8060d934
mmevy <mervemetinogluu@gmail.com>
2021-12-11 11:37:46 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+import {ToastContainer} from 'react-toastify';
+import {Routes, Route} from 'react-router-dom';
+import PrivateRoute from './routes/PrivateRoute';
+import {AuthProvider} from './auth/AuthProvider';
+import Login from './pages/Login';
+import Bill from './pages/Bill';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" exact element={<Login />} />
+        <Route
+          path="/bills"
+          element={
+            <PrivateRoute>
+              <Bill />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <ToastContainer
+        position="top-right"
+        hideProgressBar={true}
+        newestOnTop={true}
+        closeOnClick={true}
+        draggable={true}
+        pauseOnHover={true}
+      />
+    </AuthProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import './styles/index.scss';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Spinner from './components/Spinner';
+import './localization/i18next';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Suspense fallback={<Spinner />}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Suspense>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
